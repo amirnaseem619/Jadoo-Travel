5adbd3311fd15ec102b0b7b8c5e8ee3e2483abf4
amirnaseem619 <amirnaseem1235@gmail.com>
2026-04-30 11:18:22 +0500

    toggle button javascript added and Destination section also added

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const menuIcon = document.querySelector(".menu-icon");
+const menuIcon = document.querySelector(".menu-icon i");
 const navLinks = document.querySelector(".nav-links");
-
+let isOpen = false;
 menuIcon.addEventListener("click", function(){
 navLinks.classList.toggle("show");
+if(!isOpen){
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+    isOpen = true;
+}else{
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+    isOpen = false;
+}
 }
 );
