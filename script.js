05feb762f53e932d348ef90ad58c1277ce90a35c
amirnaseem619 <amirnaseem1235@gmail.com>
2026-05-11 16:24:14 +0500

    toggle added

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const closeIcon = document.getElementById('close-menu');
 const navLinks = document.querySelector('.nav-links');
 
 
-menuIcon.addEventListener('click', function(){
+menuIcon.addEventListener('click', function () {
     navLinks.classList.add('show');
     menuIcon.style.display = "none";
     closeIcon.style.display = "block";
 });
 
-closeIcon.addEventListener('click', function(){
+closeIcon.addEventListener('click', function () {
     navLinks.classList.remove('show');
     menuIcon.style.display = "block";
     closeIcon.style.display = "none";
